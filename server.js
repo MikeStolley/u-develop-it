505a3532e24531db17ae520d11d6e6d2dc0d2e3c
Mike Stolley <stolleywilliam@yahoo.com>
2022-08-18 17:24:20 -0400

    added a call to the database to communicate with server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,10 +7,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Milkmaid13579',
+        database: 'election'
+    },
+    console.log('Connected to the database.')
+);
+
 app.use((req, res) => {
     res.status(404).end();
 });
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
