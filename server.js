04d075a277963601441cae08355757d2cbe0a314
Mike Stolley <stolleywilliam@yahoo.com>
2022-09-06 16:02:58 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
-
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,15 +8,20 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Use apiRoutes
 app.use('/api', apiRoutes);
-
-
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
