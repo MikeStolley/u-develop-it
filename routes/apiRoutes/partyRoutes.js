98065ea6e1b2c76ed6192cc961e48c5af8ac99ec
Mike Stolley <stolleywilliam@yahoo.com>
2022-09-06 16:01:10 -0400

    file flow changed

---
+++ routes/apiRoutes/partyRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+
+router.get('/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+  
+router.get('/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
+
+module.exports = router;
